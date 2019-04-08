47a24fc9a814ce9206dd92e3e7ff46f32ded34a8
Nate Rauh <nrauh@users.noreply.github.com>
2019-04-07 19:42:18 -0500

    Touch up
    * Add speadsheet video
    * Better explanation for subjects
    * Working build command
    * Title

--- code_samples/03_operators.js
+++ code_samples/03_operators.js
@@ -4,7 +4,7 @@ import { debounceTime, tap, delay } from 'rxjs/operators';
 const button = document.querySelector('button');
 const output = document.querySelector('h1');
 
-const clickEvent$ = fromEvent(button, 'click')
+const clickEvent$ = fromEvent(button, 'click');
 const manipulatedSource$ = clickEvent$.pipe(
   debounceTime(200),
   tap(() => console.log('user clicked the button')),
